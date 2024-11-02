05c8290387ea2accfec9872922c2c3485f5f4349
sipahaha <xryzf28@gmail.com>
2024-11-02 20:42:59 +0700

    cobaaa

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php 
 	session_start();
 	date_default_timezone_set('Asia/Jakarta');
-	include"lib/koneksi.php";
+	include "lib/koneksi.php";
 	if (!isset($_SESSION['id'])) {
-		include"login.php";
+		include "login.php";
 	}else{
 		$iduser = $_SESSION['id'];
 		$sqlResult = $conn->query("SELECT*FROM tb_users WHERE id='$id'");
