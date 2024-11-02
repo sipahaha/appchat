41af8bcb00b77d0197c00099cd60b3e00e651ce3
sipahaha <xryzf28@gmail.com>
2024-11-02 20:37:07 +0700

    login

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php 
-	// session_start();
-	// date_default_timezone_set('Asia/Jakarta');
-	// include"lib/koneksi.php";
-	// if (!isset($_SESSION['iduser'])) {
-	// 	include"login.php";
-	// }else{
-	// 	$iduser = $_SESSION['iduser'];
-	// 	$sqlResult = $conn->query("SELECT*FROM tb_users WHERE id='$iduser'");
-	// 	$vResult = $sqlResult->fetch_array();
-	// }
+	session_start();
+	date_default_timezone_set('Asia/Jakarta');
+	include"lib/koneksi.php";
+	if (!isset($_SESSION['iduser'])) {
+		include"login.php";
+	}else{
+		$iduser = $_SESSION['iduser'];
+		$sqlResult = $conn->query("SELECT*FROM tb_users WHERE id='$iduser'");
+		$vResult = $sqlResult->fetch_array();
+	}
  ?>
