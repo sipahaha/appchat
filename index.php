1074ffaa1f4cd6162ea542d1ffdd067dd018b08a
sipahaha <xryzf28@gmail.com>
2024-11-03 21:52:29 +0700

    konek

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
-
-session_start(); // Mulai session
-// Koneksi ke database
-$servername = "localhost"; // Ganti dengan server Anda jika perlu
-$username = "root"; // Ganti dengan username database Anda
-$password = "sipa2402"; // Ganti dengan password database Anda
-$dbname = "chatapp";
-
-// Buat koneksi
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Cek koneksi
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
-}
+include "lib/koneksi.php";
 // Cek apakah pengguna sudah login
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php"); // Redirect ke halaman login jika belum login
