786b37fb11b1ab8e80216be35cac6c9bc734a4e6
Deanoviyanti <deaputrinoviyanti@gmail.com>
2024-11-03 07:22:16 +0700

    index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-kata
+<?php
+    session_start();
+    date_default_timezone_set('Asia/Jakarta');
+    include "lib/koneksi.php";
+    if (!isset($_SESSION['id'])) {
+        include "login.php";
+    }else{
+        $iduser = $_SESSION['id'];
+        $sqlResult = $conn->query("SELECT*FROM tb_users WHERE id='$id'");
+        $vResult = $sqlResult->fetch_array();
+    }
+?>
